def2e889fd67e0b6b45f1ae8f6164bb0f24b3358
Jahidul Hasan Tuhin <62537357+jhtuhin@users.noreply.github.com>
2021-10-18 09:32:32 +0600

    first commit

---
+++ Car/src/com/JHTuhin/Main.java
@@ -0,0 +1,26 @@
+package com.JHTuhin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Car car=new Car(8,"Base Car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        Mitsubishi mitsubishi=new Mitsubishi(6,"Out lander VRX 4WD");
+        System.out.println(mitsubishi.startEngine());
+        System.out.println(mitsubishi.accelerate());
+        System.out.println(mitsubishi.brake());
+
+        Holden holden=new Holden(6,"Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+
+        Ford ford=new Ford(6,"Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+    }
+}
